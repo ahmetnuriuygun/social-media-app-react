bd360394209770215394f89c6ad4b3d422e5753b
ahmetnuriuygun <ahmetnuriuygun1@gmail.com>
2023-10-28 15:29:32 +0200

    Seventh Commit: -LoginRegister page is aangemaakt

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -1,7 +1,13 @@
 import {Home} from "./Home";
 import {ACTIVE_USER, POSTS_DATA, USER_DATA} from "../data/data";
+import {Login} from "./Login";
 
 
 export function Pages(){
-    return <Home activeUser={ACTIVE_USER} users={USER_DATA} posts={POSTS_DATA} />
+    return (
+        <>
+            <Login/>
+            {/*<Home activeUser={ACTIVE_USER} users={USER_DATA} posts={POSTS_DATA} />)*/}
+        </>
+    )
 }
